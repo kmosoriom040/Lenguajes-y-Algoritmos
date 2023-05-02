a0f488b026f03ab8ab4b6b363fc4c2e96a58123c
campereza043 <129222981+campereza043@users.noreply.github.com>
2023-05-01 22:13:22 -0500

    Update Java.java

--- CPerezA/Taller/Java.java
+++ CPerezA/Taller/Java.java
@@ -1,1 +1,12 @@
-hola
+import java.util.Scanner;
+
+public class Hola_mundo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de veces que desea sea impresa la frase 'Hola mundo' ");
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Hola mundo ");
+        }
+    }
+}
