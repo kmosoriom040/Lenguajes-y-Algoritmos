0a163287fab5d643002bf1123b5e6aaac460300c
luciana666 <130503464+luciana666@users.noreply.github.com>
2023-05-02 14:52:19 -0500

    Update pYthon.py

--- LRengifoE/pYthon.py
+++ LRengifoE/pYthon.py
@@ -1,3 +1,3 @@
-n = input("Bienvenido. Ingrese el número de veces que desea que se visualice 'Hola mundo' en la pantalla ")
+n = input("Bienvenido. Ingrese el número de veces que desea que se visualice 'Hola mundo' en la pantalla (entero positivo) ")
 n = int(n)
 print("Hola mundo "*n)
