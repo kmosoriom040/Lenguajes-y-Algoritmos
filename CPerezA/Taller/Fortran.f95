400e9103744e5adcb85b62e81442161c9efdf1ab
campereza043 <129222981+campereza043@users.noreply.github.com>
2023-05-01 22:11:19 -0500

    Update Fortran.f95

--- CPerezA/Taller/Fortran.f95
+++ CPerezA/Taller/Fortran.f95
@@ -1,1 +1,9 @@
-hola
+program Hola_mundo
+    implicit none
+    integer :: n, i
+    write(*,*) "Ingrese la cantidad de veces que desea sea impresa la frase 'Hola mundo' "
+    read(*,*) n
+    do i = 1, n
+        write(*,'(a)') "Hola mundo "
+    end do
+end program Hola_mundo
