870da943200768e3318351fafce1ca855b724a86
campereza043 <129222981+campereza043@users.noreply.github.com>
2023-05-01 22:09:45 -0500

    Update C.c

--- CPerezA/Taller/C.c
+++ CPerezA/Taller/C.c
@@ -1,1 +1,11 @@
-hola
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Ingrese la cantidad de veces que desea sea impresa la frase 'Hola mundo'");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Hola mundo");
+    }
+    return 0;
+}
