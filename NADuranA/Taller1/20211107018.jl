4e7af22524f5e2d5264033f274c716f7b7d02067
NADURANA <130503412+NADURANA@users.noreply.github.com>
2023-05-09 15:06:03 -0500

    Create 20211107018.jl

---
+++ NADuranA/Taller1/20211107018.jl
@@ -0,0 +1,5 @@
+println("Escriba el numero de 'Hola Mundo!' que desee ver en pantalla en forma de valor numerico: ")
+    n = parse(Int, readline())
+    for i in 1:n
+        print("Hola mundo ")
+end
