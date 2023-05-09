124af41bdb16f12199d0abc86a14c4b5ec708245
NADURANA <130503412+NADURANA@users.noreply.github.com>
2023-05-09 15:00:41 -0500

    Update 20211107018.py

--- NADuranA/Taller1/20211107018.py
+++ NADuranA/Taller1/20211107018.py
@@ -1,3 +1,3 @@
-n = input("Escriba el numero de 'Hola Mundo!' que desee ver en pantalla en forma de valor numerico")
+n = input("Escriba el numero de 'Hola Mundo!' que desee ver en pantalla en forma de valor numerico: ")
 n = int(n)
 print("Hola Mundo!"*n)
