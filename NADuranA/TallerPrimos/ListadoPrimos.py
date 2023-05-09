e7df7e488d690772c873852fb0ff552416db9a28
NADURANA <130503412+NADURANA@users.noreply.github.com>
2023-05-09 14:53:47 -0500

    Update ListadoPrimos.py

--- NADuranA/TallerPrimos/ListadoPrimos.py
+++ NADuranA/TallerPrimos/ListadoPrimos.py
@@ -1,1 +1,8 @@
-
+num = int(input("Ingrese el valor del punto maximo del rango para calcular primos: ")
+ 
+ for i in range(2, num+1):
+          for j in range(2,i):
+           if i % j == 0:
+            break
+          else:
+           print(f"(i) es primo")
