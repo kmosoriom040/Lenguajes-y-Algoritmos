4e5b846b32b26818521084ae292c477d1459447c
luciana666 <130503464+luciana666@users.noreply.github.com>
2023-05-09 14:25:10 -0500

    Update and rename programajulia.jl to 20211107039.jl

--- LRengifoE/Taller/20211107039.jl
+++ LRengifoE/Taller/20211107039.jl
@@ -1,5 +1,5 @@
 println("Hola, ingrese la cantidad de 'Hola mundo' que desea visulaizar en pantalla ")
-n = parse(Int, readline())
-for i in 1:n
-    print("Hola mundo ")
+    n = parse(Int, readline())
+    for i in 1:n
+        print("Hola mundo ")
 end
